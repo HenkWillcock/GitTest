be4e90ac0c1b2a84da4bbff227668f0564e7e603
ACER <ACER@ACER-PC>
2018-03-13 10:21:12 +1300

    Software deez titties

---
+++ GitTest/src/main/DeezNutter.java
@@ -0,0 +1,11 @@
+package main;
+
+public class DeezNutter {
+
+	public void nutEverywhere() {
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Sperm");
+		}
+	}
+
+}
---
+++ GitTest/src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Main {
+	public static void main(String args[]) {
+		DeezNutter nutter = new DeezNutter();
+		nutter.nutEverywhere();
+	} 
+}
